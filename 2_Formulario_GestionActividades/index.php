3aedf177967a31c23b5e2ba51a36f10ad11c4537
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-03-29 18:03:46 +0200

    V1 mostrando los resultados hecho

---
+++ 2_Formulario_GestionActividades/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Juan Manuel Toscano Reyes">
+        <title>Gestion Actividades</title>
+    </head>
+    <body>
+        <form action="#" method="post">
+            <select name="categoria">
+                <option value="navidad">Navidad</option>
+                <option value="semanaIgnaciana">Semana Ignaciana</option>
+                <option value="fiestasEscolares">Fiestas Escolares</option>
+            </select><br />
+            <br />
+            <label>Nombre Actividad :</label>
+            <input type="text" name="nombreActividad" placeholder="Nombre Actividad"><br />
+            <br />
+            <label>ETAPAS:</label> <br />
+            <label>Primaria </label>
+            <input type="checkbox" name="primaria"><br />
+            <label>ESO </label>
+            <input type="checkbox" name="eso"><br />
+            <label>Bachillerato </label>
+            <input type="checkbox" name="bachillerato"><br />
+            <label>CFGM</label>
+            <input type="checkbox" name="cfgm"><br />
+            <label>CFGS </label>
+            <input type="checkbox" name="cfgs"><br />
+            <br />
+            <label>Actividad de Sección: </label>
+            <input type="checkbox" name="actividadSeccion"><br />
+            <br />
+            <input type="submit" value="Enviar" name="enviar">
+        </form>
+        <?php
+            if(isset($_POST['enviar'])){
+                foreach($_POST as $nombre=>$valor){
+                    echo "$nombre : $valor<br>";
+                }
+            }
+        ?>
+    </body>
+</html>
