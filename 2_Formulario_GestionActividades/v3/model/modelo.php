b02c09ca1b6ff6a2506ec2373685f9316d65b3aa
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-04-04 08:40:02 +0200

    V3 terminado

--- 2_Formulario_GestionActividades/v3/model/modelo.php
+++ 2_Formulario_GestionActividades/v3/model/modelo.php
@@ -16,6 +16,8 @@
          */
         function recoger_formulario(){
             if(isset($_POST["enviar"])){
+                /* Elimino el campo enviar del formulario para que no lo muestre */
+                unset($_POST["enviar"]);
                 foreach($_POST as $nombre=>$valor){
                     $formulario["$nombre"]=$valor;
                 }
@@ -28,28 +30,24 @@
          * @param $formulario
          */
         function mostrar_datos($formulario){
-            if(isset($formulario['enviar'])){
-                /* Elimino el campo enviar del formulario para que no lo muestre */
-                unset($formulario["enviar"]);
-                /* Recorro el array formulario con un foreach */
-                foreach($formulario as $nombre=>$valor){
-                    /* Compruebo si está en el campo "nombreActividad" y si está vacío pues no hago nada */
-                    if(empty($formulario["nombreActividad"]) && $formulario["nombreActividad"]==$formulario["$nombre"]){
-                        echo "Debe introducir un nombre en la actividad <br>";
+            /* Recorro el array formulario con un foreach */
+            foreach($formulario as $nombre=>$valor){
+                /* Compruebo si está en el campo "nombreActividad" y si está vacío pues no hago nada */
+                if(empty($formulario["nombreActividad"]) && $formulario["nombreActividad"]==$formulario["$nombre"]){
+                    echo "Debe introducir un nombre en la actividad <br>";
+                }else{
+                    /* Compruebo si está en el campo "etapas" y si existe a la vez y solo muestro el valor del campo */
+                    if(isset($formulario["etapas"]) && $formulario["etapas"]==$formulario["$nombre"]){
+                        foreach($formulario["etapas"] as $valor){
+                            echo "$valor <br>";
+                        }
                     }else{
-                        /* Compruebo si está en el campo "etapas" y si existe a la vez y solo muestro el valor del campo */
-                        if(isset($formulario["etapas"]) && $formulario["etapas"]==$formulario["$nombre"]){
-                            foreach($formulario["etapas"] as $valor){
-                                echo "$valor <br>";
-                            }
+                        /* Compruebo si está en el campo "actividadSeccion" y si existe a la vez y solo muestro el nombre de actividadSeccion */
+                        if(isset($formulario["actividadSeccion"]) && $formulario["actividadSeccion"]==$formulario["$nombre"]){
+                            echo "actividadSeccion <br>";
                         }else{
-                            /* Compruebo si está en el campo "actividadSeccion" y si existe a la vez y solo muestro el nombre de actividadSeccion */
-                            if(isset($formulario["actividadSeccion"]) && $formulario["actividadSeccion"]==$formulario["$nombre"]){
-                                echo "actividadSeccion <br>";
-                            }else{
-                                /* Si no se cumple nada de lo anterior pues muestro el nombre del campo con su valor */
-                                echo "$nombre: $valor<br>";
-                            }
+                            /* Si no se cumple nada de lo anterior pues muestro el nombre del campo con su valor */
+                            echo "$nombre: $valor<br>";
                         }
                     }
                 }
