ab3128ffdb817be580fd378a54f5f090fd06d141
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-04-05 13:56:39 +0200

    Ejercicio 1 con POO hecho

--- 1_Arrays_EjIniciales/POO/index.php
+++ 1_Arrays_EjIniciales/POO/index.php
@@ -12,8 +12,11 @@
         <h1>Foreach</h1>
         <h3>Foreach con los meses del año y su valor de dias que le corresponde a cada uno</h3>
         <?php
+            /* LLamo al archivo modelo.php */
             require_once __DIR__. "/model/modelo.php";
+            /* Instancio el objeto */
             $modelo=new Modelo;
+            /* Llamo a los metodos del objeto */
             $modelo->darValores();
             $modelo->verValores();
         ?>
