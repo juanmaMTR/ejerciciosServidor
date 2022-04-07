3262f7ec5b9449d10e13dd03e4ca5b172504da00
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-04-07 10:26:09 +0200

    EjArray con POO hecho

---
+++ 1_Arrays_EjIniciales/POO/prueba3.2/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!-- Llamamos a los métodos crear_array desde el archivo externo y desde este mismo archivo mostramos el array (NO usamos el método visualizar) -->
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Juan Manuel Toscano Reyes">
+        <title>Foreach</title>
+    </head>
+    <body>
+        <h1>Foreach</h1>
+        <h3>Foreach con los meses del año y su valor de dias que le corresponde a cada uno</h3>
+        <?php
+            /* LLamo al archivo modelo.php */
+            require_once __DIR__. "/model/modelo.php";
+            /* Instancio el objeto */
+            $modelo=new Modelo;
+            /* Instancio las variables que retorno del método darValores() */
+            //print_r ($modelo->darValores());
+            $meses=$modelo->darValores()[0];
+            $diasemana=$modelo->darValores()[1];
+            /* Recorro el array $meses y muestro los datos mediante un echo en el foreach */
+            foreach($meses as $nombremes=>$dia){
+                echo "Mes: $nombremes => $dia.<br>";
+            }
+            echo "<h3>Foreach solo valor</h3>";
+            /* Recorro el array para mostrar solo el valor */
+            foreach($meses as $dia){
+                echo "Valor $dia<br>";
+            }
+            echo "<h3>Foreach dias de la semana con el indice</h3>";
+            /* Sumo uno al indice porque empieza en la posicion 0 */
+            foreach($diasemana as $indice=>$valor){
+                echo "Dia[". $indice+1 ."]: $valor<br>";
+            }
+            echo "<h3>For utilizando el count</h3>";
+            /* Sumo uno al indice porque empieza en la posicion 0, y el count te dice la cantidad de elementos que tiene el array */
+            for ($i=0; $i < count($diasemana); $i++) { 
+                echo "Dia[". $i+1 ."]: ". $diasemana[$i] ."<br>";
+            } 
+        ?>
+    </body>
+</html>
---
+++ 1_Arrays_EjIniciales/POO/prueba3.2/model/modelo.php
@@ -0,0 +1,35 @@
+<?php
+    class Modelo{
+        /* Deben de ser publicos porque los llamo desde un archivo externo */
+        private $meses;
+        private $diasemana;
+        function darValores(){
+            /* Creo la valiable meses que va a ser un array con los meses y el valor de los días */
+            $this->meses=array(
+                "enero"=>31,
+                "febrero"=>28,
+                "marzo"=>31,
+                "abril"=>30,
+                "mayo"=>31,
+                "junio"=>30,
+                "julio"=>31,
+                "agosto"=>31,
+                "septiembre"=>30,
+                "octubre"=>31,
+                "noviembre"=>30,
+                "diembre"=>31
+            );
+            $this->diasemana[]="lunes";
+            $this->diasemana[]="martes";
+            $this->diasemana[]="miercoles";
+            $this->diasemana[]="jueves";
+            $this->diasemana[]="viernes";
+            $this->diasemana[]="sabado";
+            $this->diasemana[]="domingo";
+            
+            $variables[]=$this->meses;
+            $variables[]=$this->diasemana;
+            return $variables;
+        }
+    }
+/* FIN DE LA PAGINA */
